f82d83d1795ba1861d473b652426a4846ae86576
Sebastian420pgl <joansebastian2712@gmail.com>
2019-08-27 11:45:51 -0500

    Conexion base de datos y llamar datos a php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| BARATON
+|--------------------------------------------------------------------------
+| Author: Sistema de inventario 5.0
+| Project Name: BARATON
+| Version: v1
+| 
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+
+?>
